14e9a1fc17c18ea59d55e7f0fc10fb08b80ec8be
Shoaib Shaikh <128297159+shoaibshaikh07@users.noreply.github.com>
2025-05-21 11:55:30 -0400

    feat: it worked, updated backend docker

---
+++ apps/client/src/middleware.ts
@@ -0,0 +1,61 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { api } from "./lib/utils";
+
+export async function middleware(request: NextRequest): Promise<NextResponse> {
+  if (request.nextUrl.pathname === "/auth") {
+    return NextResponse.next();
+  }
+
+  const cookie = request.headers.get("cookie") || "";
+
+  const response = await api.get("/auth/get-session", {
+    headers: {
+      Cookie: cookie,
+    },
+  });
+
+  if (!response.data) {
+    return NextResponse.redirect(new URL("/auth", request.url));
+  }
+
+  // Check onboarding status once
+  const checkOnboardingStatus = await api.get("/user/onboarding", {
+    headers: {
+      Cookie: cookie,
+    },
+  });
+
+  const isOnboardingCompleted =
+    checkOnboardingStatus.data.success && checkOnboardingStatus.data.completed;
+
+  // Handle auth path
+  if (request.nextUrl.pathname.startsWith("/auth")) {
+    if (isOnboardingCompleted) {
+      return NextResponse.redirect(new URL("/jobs", request.url));
+    }
+    return NextResponse.redirect(new URL("/onboarding", request.url));
+  }
+
+  // Handle onboarding path
+  if (request.nextUrl.pathname.startsWith("/onboarding")) {
+    if (isOnboardingCompleted) {
+      return NextResponse.redirect(new URL("/jobs", request.url));
+    }
+    return NextResponse.next();
+  }
+
+  // Handle jobs path
+  if (request.nextUrl.pathname.startsWith("/jobs")) {
+    if (!isOnboardingCompleted) {
+      return NextResponse.redirect(new URL("/onboarding", request.url));
+    }
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/auth/:path*", "/onboarding/:path*", "/jobs/:path*"],
+};
